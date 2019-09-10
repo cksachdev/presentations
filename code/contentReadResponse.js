2bcdde16f4c789111b8a49672a4c9cb450cf2ac6
Nikunj Beladiya <nikunj.beladiya@tarento.com>
2019-09-10 15:18:54 +0530

    Documented content update and read apis

---
+++ code/contentReadResponse.js
@@ -0,0 +1,187 @@
+"result": {
+    "content": {
+        "ownershipType": [
+            "createdBy"
+        ],
+        "copyright": "First Org",
+        "keywords": [ "jadav",  " Sister" ],
+        "year": "2004",
+        "subject": "History",
+        "downloadUrl": "",
+        "channel": "0126825293972439041",
+        "organisation": [
+            "First Org"
+        ],
+        "language": [
+            "English"
+        ],
+        "variants": {
+            "online": {
+                "ecarUrl": "",
+                "size": 16289
+            },
+            "spine": {
+                "ecarUrl": "",
+                "size": 389341
+            }
+        },
+        "mimeType": "application/vnd.ekstep.content-collection",
+        "leafNodes": [
+            "do_21284314348276940816569",
+            "do_30099034",
+            "do_30099045",
+            "do_21284314350809907216572",
+            "do_30098973",
+        ],
+        "appIcon": "",
+        "gradeLevel": [
+            "Class 7",
+            "Class 9"
+        ],
+        "collections": [],
+        "children": [
+            {
+                "identifier": "do_21284308055605248016475",
+                "name": "TESTING COLLECTION",
+                "objectType": "Content",
+                "relation": "hasSequenceMember",
+                "description": "TestCollection Description Desctiption",
+                "index": 1,
+                "status": "Live",
+                "depth": null,
+                "mimeType": null,
+                "visibility": null,
+                "compatibilityLevel": null
+            },
+            {
+                "identifier": "do_31257981699854336019657",
+                "name": "Our First School_2",
+                "objectType": "Content",
+                "relation": "hasSequenceMember",
+                "description": "Looking Around_4_Our First School_OUR FAMILY",
+                "index": 1,
+                "status": "Live",
+                "depth": null,
+                "mimeType": null,
+                "visibility": null,
+                "compatibilityLevel": null
+            }
+        ],
+        "appId": "preprod.diksha.portal",
+        "contentEncoding": "gzip",
+        "lockKey": "49c721da-f0d4-4336-954b-edc6c26102a7",
+        "mimeTypesCount": "{\"application/vnd.ekstep.content-collection\":7,\"application/vnd.ekstep.ecml-archive\":8,\"video/x-youtube\":1}",
+        "totalCompressedSize": 34426338,
+        "contentType": "TextBook",
+        "lastUpdatedBy": "0b96635f-fe2b-4ab0-a511-05cfce8faa3f",
+        "identifier": "do_21284245547547033616364",
+        "audience": [
+            "Learner"
+        ],
+        "toc_url": "",
+        "visibility": "Default",
+        "contentTypesCount": "{\"TextBookUnit\":4,\"Resource\":9,\"Collection\":3}",
+        "childNodes": [
+            "do_21284245946742374416365",
+            "do_21284246452998963216370",
+            "do_31257981699854336019657",
+            "do_21284314348276940816569",
+            "do_30099034",
+            "do_30099045",
+            "do_21284314350809907216572",
+            "do_30098973",
+        ],
+        "consumerId": "2eaff3db-cdd1-42e5-a611-bebbf906e6cf",
+        "mediaType": "content",
+        "ageGroup": [
+            "6-7"
+        ],
+        "osId": "org.ekstep.quiz.app",
+        "lastPublishedBy": "30b2571f-08f9-49ce-b97a-c643df0c82f7",
+        "version": 2,
+        "tags": [ "Read-Aloud Stories",  "story", "child"  ],
+        "prevState": "Review",
+        "license": "Creative Commons Attribution (CC BY)",
+        "qrCodeProcessId": "c875b65d-2b71-4580-9f70-27b94f4334ad",
+        "size": 389341,
+        "lastPublishedOn": "2019-09-09T08:41:47.975+0000",
+        "concepts": [
+            {
+                "identifier": "LO7",
+                "name": "Decoding for Reading",
+                "objectType": "Concept",
+                "relation": "associatedTo",
+                "description": "Decoding for Reading",
+                "index": null,
+                "status": "Live",
+                "depth": null,
+                "mimeType": null,
+                "visibility": null,
+                "compatibilityLevel": null
+            },
+            {
+                "identifier": "LO53",
+                "name": "Word Context",
+                "objectType": "Concept",
+                "relation": "associatedTo",
+                "description": "Accurate Word For Context",
+                "index": null,
+                "status": "Live",
+                "depth": null,
+                "mimeType": null,
+                "visibility": null,
+                "compatibilityLevel": null
+            }
+        ],
+        "domain": [
+            "literacy"
+        ],
+        "name": "Textbook 1 test",
+        "topic": [
+            "Environment",
+            "Media",
+            "Water"
+        ],
+        "publisher": "Sinha",
+        "status": "Live",
+        "code": "org.sunbird.W4HWGY",
+        "description": "Enter description for TextBook",
+        "medium": "English",
+        "posterImage": "",
+        "idealScreenSize": "normal",
+        "createdOn": "2019-09-05T11:22:33.157+0000",
+        "reservedDialcodes": {
+            "U2Q6A5": 3,
+            "Z5G4G5": 1
+        },
+        "contentDisposition": "inline",
+        "lastUpdatedOn": "2019-09-09T08:41:35.682+0000",
+        "SYS_INTERNAL_LAST_UPDATED_ON": "2019-09-09T08:41:49.041+0000",
+        "dialcodeRequired": "No",
+        "createdFor": [
+            "0126825293972439041"
+        ],
+        "lastStatusChangedOn": "2019-09-09T08:41:35.653+0000",
+        "creator": "Content Creator",
+        "os": [
+            "All"
+        ],
+        "pkgVersion": 7,
+        "versionKey": "1568018501175",
+        "idealScreenDensity": "hdpi",
+        "dialcodes": [
+            "X2N1L2"
+        ],
+        "depth": 0,
+        "s3Key": "ecar_files/do_21284245547547033616364/textbook-1-test_1568018508332_do_21284245547547033616364_7.0_spine.ecar",
+        "framework": "ekstep_ncert_k-12",
+        "lastSubmittedOn": "2019-09-09T08:40:22.746+0000",
+        "createdBy": "0b96635f-fe2b-4ab0-a511-05cfce8faa3f",
+        "leafNodesCount": 9,
+        "compatibilityLevel": 1,
+        "usedByContent": [],
+        "board": "CBSE",
+        "resourceType": "Book",
+        "languageCode": "en"
+    }
+}
---
+++ code/updateContent.js
@@ -0,0 +1,28 @@
+{
+    "request": {
+      "content": {
+        "versionKey": "1529042067611",
+        "body": "Content body for content. Only for ECML content",
+        "contentType": "Resource",
+        "resourceType": "Learn",
+        "name": "TestBook api",
+        "description": "KNOWING OUR NUMBERS",
+        "framework": "NCF",
+        "board": "State (Karnataka)",
+        "medium": "Hindi",
+        "gradeLevel": [
+        "Class 6"
+        ],
+        "subject": "Mathematics",
+        "createdBy": "50c7d6a9-ff0e-4efd-9f8e-b57c9c6f95b5",
+        "organisation": [
+        "aprTesttwo"
+        ],
+        "createdFor": [
+        "014624758449502453761"
+        ],
+        "mimeType": "application/vnd.ekstep.ecml-archive",
+        "creator": "श्याम प्रताप सिंह",
+      }
+    }
+  }
